cd400a6ebe1d8f38f5a21adaf6ad13bfa8697d4c
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-03-30 00:46:05 -0300

    update version capivara

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,39 @@
 */
 
 import capivara from 'capivarajs';
+
 import { ProjectName } from './component/project-name.component';
 
 const Component = {
+    /**
+     * @name template
+     * @description Applies the visual part of the component
+     */
     template  : require('./component/project-name.template.html'),
+    /**
+     * @name style
+     * @description Import component style
+     */
     style     : require('./component/project-name.style.scss'),
+    /**
+     * @name constants
+     * @description Declares the constants that will be accepted by component. See https://capivarajs.github.io/components.html#constants
+     */
+    constants: [],
+    /**
+     * @name functions
+     * @description Declares the functions that will be accepted by component. See https://capivarajs.github.io/components.html#functions
+     */
+    functions: [],
+    /**
+     * @name bindings
+     * @description Declares the variables that will be accepted by component. See https://capivarajs.github.io/components.html#bindings
+     */
+    bindings: [],
+    /**
+     * @name controller
+     * @description Sets the scope of the component
+     */
     controller: ProjectName
 };
 
